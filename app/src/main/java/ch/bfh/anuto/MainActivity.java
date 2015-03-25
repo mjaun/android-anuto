12d786289bf0b2bd18898271d23ec77b9b0ed605
oojeiph <oojeiph01@gmail.com>
2015-03-25 07:14:22 +0100

    removed menu bar

--- app/src/main/java/ch/bfh/anuto/MainActivity.java
+++ app/src/main/java/ch/bfh/anuto/MainActivity.java
@@ -2,9 +2,6 @@ package ch.bfh.anuto;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
 
 public class MainActivity extends ActionBarActivity {
 
@@ -16,24 +13,6 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         tdView = (TowerDefenseView)findViewById(R.id.td_view);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
