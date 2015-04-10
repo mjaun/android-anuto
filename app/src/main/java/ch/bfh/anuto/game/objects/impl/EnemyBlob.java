1fb126343e5746f94f39022fdfda0a3b2c43b49a
id101010 <aaron@duckpond.ch>
2015-04-11 01:52:10 +0200

    Sprinter test

--- app/src/main/java/ch/bfh/anuto/game/objects/impl/EnemyBlob.java
+++ app/src/main/java/ch/bfh/anuto/game/objects/impl/EnemyBlob.java
@@ -2,19 +2,43 @@ package ch.bfh.anuto.game.objects.impl;
 
 import android.content.res.Resources;
 
+import ch.bfh.anuto.R;
+import ch.bfh.anuto.game.GameEngine;
+import ch.bfh.anuto.game.Sprite;
 import ch.bfh.anuto.game.objects.Enemy;
 
 public class EnemyBlob extends Enemy {
 
-    // To be implemented ...
+    private final static int HEALTH = 10000;
+    private final static float MOVEMENT_SPEED = 1f / GameEngine.TARGET_FPS;
+    private final static float ANIMATION_SPEED = 1.5f / GameEngine.TARGET_FPS;
+
+    private Sprite mSprite;
+
+    public EnemyBlob() {
+        mHealth = mHealthMax = HEALTH;
+        mSpeed = MOVEMENT_SPEED;
+    }
 
     @Override
     public void init(Resources res) {
+        super.init(res);
 
+        mSprite = Sprite.fromResources(this, res, R.drawable.blob_enemy, 9);
+        mSprite.calcMatrix(0.9f);
+        mGame.addDrawObject(mSprite, LAYER);
     }
 
     @Override
     public void clean() {
+        super.clean();
 
+        mGame.removeDrawObject(mSprite);
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        mSprite.cycle2(ANIMATION_SPEED);
     }
 }
--- app/src/main/java/ch/bfh/anuto/game/objects/impl/EnemySprinter.java
+++ app/src/main/java/ch/bfh/anuto/game/objects/impl/EnemySprinter.java
@@ -2,18 +2,44 @@ package ch.bfh.anuto.game.objects.impl;
 
 import android.content.res.Resources;
 
+import ch.bfh.anuto.R;
+import ch.bfh.anuto.game.GameEngine;
+import ch.bfh.anuto.game.Sprite;
 import ch.bfh.anuto.game.objects.Enemy;
 
 public class EnemySprinter extends Enemy {
 
-    // To be implemented ...
+    private final static int HEALTH = 500;
+    private final static float MOVEMENT_SPEED = 3f / GameEngine.TARGET_FPS;
+    private final static float ANIMATION_SPEED = 1.5f / GameEngine.TARGET_FPS;
+
+    private float mAngle;
+    private Sprite mSprite;
+
+    public EnemySprinter() {
+        mHealth = mHealthMax = HEALTH;
+        mSpeed = MOVEMENT_SPEED;
+    }
 
     @Override
     public void init(Resources res) {
+        super.init(res);
+
+        mSprite = Sprite.fromResources(this, res, R.drawable.sprinter_enemy, 1);
+        mSprite.calcMatrix(0.9f);
+        mGame.addDrawObject(mSprite, LAYER);
     }
 
     @Override
     public void clean() {
+        super.clean();
 
+        mGame.removeDrawObject(mSprite);
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        mSprite.cycle2(ANIMATION_SPEED);
     }
 }
