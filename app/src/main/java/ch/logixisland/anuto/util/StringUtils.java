5298c1f5ab1e6699cfdb294eccdce98436c74ee9
reloZid <relozid@logixisland.ch>
2015-10-02 16:43:08 +0200

    optimized number format

--- app/src/main/java/ch/logixisland/anuto/util/StringUtils.java
+++ app/src/main/java/ch/logixisland/anuto/util/StringUtils.java
@@ -20,21 +20,25 @@ public final class StringUtils {
 
     public static String formatSuffix(float number, boolean integer) {
         String suffix = "";
+        boolean big = false;
 
         if (number >= 1e10f) {
             suffix = "G";
             number /= 1e9f;
+            big = true;
         }
         else if (number >= 1e7f) {
             suffix = "M";
             number /= 1e6f;
+            big = true;
         }
         else if (number >= 1e4f) {
             suffix = "k";
             number /= 1e3f;
+            big = true;
         }
 
-        DecimalFormat fmt = (number < 1e2f && !integer) ? fmt1 : fmt0;
+        DecimalFormat fmt = (number < 1e2f && (!integer || big)) ? fmt1 : fmt0;
         return fmt.format(number) + suffix;
     }
 }
