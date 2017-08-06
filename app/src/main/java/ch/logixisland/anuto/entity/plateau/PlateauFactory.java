0a8060eeae751e1881693e8c6beeb245980b4456
reloZid <relozid@logixisland.ch>
2017-08-06 18:47:37 +0200

    remove generic factory from plateau factory

--- app/src/main/java/ch/logixisland/anuto/entity/plateau/PlateauFactory.java
+++ app/src/main/java/ch/logixisland/anuto/entity/plateau/PlateauFactory.java
@@ -1,21 +1,22 @@
 package ch.logixisland.anuto.entity.plateau;
 
 import ch.logixisland.anuto.engine.logic.GameEngine;
-import ch.logixisland.anuto.util.GenericFactory;
 
 public class PlateauFactory {
 
     private final GameEngine mGameEngine;
-    private final GenericFactory<Plateau> mFactory;
 
     public PlateauFactory(GameEngine gameEngine) {
         mGameEngine = gameEngine;
-        mFactory = new GenericFactory<>(GameEngine.class);
-        mFactory.registerClass(BasicPlateau.class);
     }
 
     public Plateau createPlateau(String name) {
-        return mFactory.createInstance(name, mGameEngine);
+        switch (name) {
+            case "basic":
+                return new BasicPlateau(mGameEngine);
+            default:
+                throw new IllegalArgumentException("Unknown plateau name!");
+        }
     }
 
 }
