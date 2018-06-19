d49ded58820cd2e5f3f989fa9625a5311a2fed76
reloZid <relozid@logixisland.ch>
2018-06-19 21:58:17 +0200

    remove unused classes

--- app/src/main/java/ch/logixisland/anuto/util/serializer/Converter.java
+++
@@ -1,8 +0,0 @@
-package ch.logixisland.anuto.util.serializer;
-
-import java.lang.reflect.Type;
-
-public interface Converter<T> {
-    Object write(T value) throws SerializerException;
-    T read(Type type, Object json) throws SerializerException;
-}
