2a51b4b92abcb6e126dc9965b11ae500fa783f8e
reloZid <relozid@logixisland.ch>
2018-07-21 17:06:04 +0200

    add adaptive icon (API 26+)

--- images/generate.bat
+++ images/generate.bat
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 inkscape -f icon.svg -e icon.png -a 0:0:512:512 -w 512 -h 512
 inkscape -f icon.svg -e ../app/src/main/res/mipmap-mdpi/icon.png -a 0:0:512:512 -w 48 -h 48
 inkscape -f icon.svg -e ../app/src/main/res/mipmap-hdpi/icon.png -a 0:0:512:512 -w 72 -h 72
