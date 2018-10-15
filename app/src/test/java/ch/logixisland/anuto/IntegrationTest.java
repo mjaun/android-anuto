432b6c8db6e85c7062b0ae27f05d220bfbf88439
reloZid <relozid@logixisland.ch>
2018-10-15 17:25:17 +0200

    add failing test to test travis

---
+++ app/src/test/java/ch/logixisland/anuto/IntegrationTest.java
@@ -0,0 +1,12 @@
+package ch.logixisland.anuto;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class IntegrationTest {
+    @Test
+    public void integrationTest() {
+        assertTrue(false);
+    }
+}
